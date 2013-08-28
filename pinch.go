8e679d0b6a2a5f7ac5d6c030a43e6abb07161532
Peter Hellberg <peter@c7.se>
2013-08-28 22:21:19 +0200

    pinch.Get now takes two strings Support for getContentLength.

--- pinch.go
+++ pinch.go
@@ -2,10 +2,31 @@ package pinch
 
 import (
 	_ "compress/zlib"
-	_ "net/http"
-	"net/url"
+	"fmt"
+	"net/http"
 )
 
-func Get(url *url.URL, fn string) string {
-	return ""
+func Get(url string, fn string) {
+	of := int64(0)
+	cl := getContentLength(url)
+
+	if cl >= 4096 {
+		of = cl - 4096
+	}
+
+	fmt.Println("URL            ", url)
+	fmt.Println("File name      ", fn)
+	fmt.Println("Content-Length ", cl)
+	fmt.Println("Offset         ", of)
+}
+
+func getContentLength(url string) int64 {
+	resp, err := http.Head(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return resp.ContentLength
+}
+
+func getData(sof int64, eof int64) {
 }
--- pinch/main.go
+++ pinch/main.go
@@ -5,25 +5,26 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"strings"
 )
 
 func main() {
-	url, fn = getArgs(os.Args)
-
-	pinch.Get(url, fn)
+	pinch.Get(getArgs(os.Args))
 }
 
-func getArgs(args []string) (*url.URL, string) {
+func getArgs(args []string) (string, string) {
 	// Make sure that we got three command line arguments
 	if len(args) != 3 {
 		log.Fatalln("Usage: pinch <url> <file>")
 	}
 
 	// Parse the URI parameter
-	var url, err = url.ParseRequestURI(args[1])
-	if err != nil {
+	_, err := url.ParseRequestURI(args[1])
+
+	if err != nil || !strings.HasPrefix(args[1], "http") {
 		log.Fatalln("Invalid URL")
 	}
 
-	return url, args[2]
+	// Return the two arguments
+	return args[1], args[2]
 }
