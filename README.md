5a4da83d29b6790b9581a9c4e534d0a88e6f4e69
Edward Patel <edward.patel@memention.com>
2013-12-29 20:16:26 +0100

    Added code

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # Go Pinch
 
-*TODO:* Actually write the code.
+*TODO:* Clean up the code.
 
 Retrieve a file from inside a zip file, over the network!
 
 Pinch makes it possible to download a specific file from within 
 a ZIP file over HTTP/1.1, using nothing but the Go Standard 
 Library ([net/http](http://golang.org/pkg/net/http/) and 
-[compress/zlib](http://golang.org/pkg/compress/zlib/))
+[compress/flate](http://golang.org/pkg/compress/flate/))
 
-The first version was written in Objective-C and we thought it 
-would be cool if we could bring that functionality to Ruby, 
-I’m now about to translate the code to Go :)
+Earlier versions was written in [Objective-C](https://github.com/epatel/pinch-objc) and [Ruby](https://github.com/peterhellberg/pinch)
 
 ## Usage
 
 ```bash
-$ pinch http://example/path/to.zip file.json
+$ ./pinch http://example/path/to.zip file.json
 ```
 
 Or from Go directly:
@@ -25,13 +23,14 @@ Or from Go directly:
 package main
 
 import (
-  "github.com/peterhellberg/go-pinch"
-  "net/url"
-  )
+  "go-pinch"
+)
 
 func main() {
-  url, _ = uri.ParseRequestURI("http://example/path/to.zip")
   
-  pinch.Get(url, "file.json")
+  entries, err := pinch.GetZipDirectory("http://example/path/to.zip")
+
+  file, err := pinch.GetZipFile(url, entries["file.json"])
+
 }
 ```
--- pinch.go
+++ pinch.go
@@ -1,32 +1,99 @@
-package pinch
+/*---------------------------------------------------------------------------
+ 
+ Copyright (c) 2013 Peter Hellberg, Edward Patel
+ 
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ ---------------------------------------------------------------------------*/
+
+package main
 
 import (
-	_ "compress/zlib"
+	"go-pinch"
+	"os"
 	"fmt"
-	"net/http"
+	"log"
+	"net/url"
+	"strings"
 )
 
-func Get(url string, fn string) {
-	of := int64(0)
-	cl := getContentLength(url)
+func main() {
 
-	if cl >= 4096 {
-		of = cl - 4096
-	}
+	args := getArgs(os.Args)
 
-	fmt.Println("URL            ", url)
-	fmt.Println("File name      ", fn)
-	fmt.Println("Content-Length ", cl)
-	fmt.Println("Offset         ", of)
-}
+	url := args[0]
 
-func getContentLength(url string) int64 {
-	resp, err := http.Head(url)
+	entries, err := pinch.GetZipDirectory(url)
 	if err != nil {
 		fmt.Println(err)
+		log.Fatalln("exiting")
 	}
-	return resp.ContentLength
+
+	if len(args) == 2 {
+		
+		var entry pinch.ZipEntry = entries[args[1]]
+
+		if len(entry.Filename) > 0 {
+
+			file, err := pinch.GetZipFile(url, entry)
+			if err != nil {
+				fmt.Println(err)
+				log.Fatalln("exiting")
+			}
+
+			os.Stdout.Write(file)
+
+		} else {
+
+			fmt.Printf("File not found\n")
+		
+		}
+	
+	} else {
+
+		for _, entry := range entries {
+			fmt.Println(entry.Filename)
+		}
+
+	}
 }
 
-func getData(sof int64, eof int64) {
+func getArgs(args []string) []string {
+
+	// Make sure that we got three command line arguments
+	if len(args) < 2 || len(args) > 3 {
+		fmt.Println("Usage: pinch <url> [ <file> ]")
+		log.Fatalln("exiting")
+	}
+
+	// Parse the URI parameter
+	_, err := url.ParseRequestURI(args[1])
+
+	if err != nil || !strings.HasPrefix(args[1], "http") {
+		fmt.Println("Invalid URL")
+		log.Fatalln("exiting")
+	}
+
+	if len(args) == 2 {
+		return []string{ args[1] }
+	}
+
+	return []string{ args[1], args[2] }
 }
